ac97c4788a0929b6f5c4a7c967e9688a3e6640f5
Pranav <pvanparty@gmail.com>
2026-06-06 17:26:42 -0400

    Made proper calibration code

---
+++ Deploy/CalibrateServo.py
@@ -0,0 +1,36 @@
+import json
+import time
+
+from adafruit_servokit import ServoKit
+
+
+def calibrateServo(kit: ServoKit):
+    name = input("Enter the name of the servo: ")
+    slot = input("Enter the slot of the servo: ")
+    slot = int(slot)
+
+    # This sweeps through 1480-1550µs in 10µ steps
+    for stop_pulse in range(1480, 1560, 10):
+        min_pulse = 600
+        max_pulse = 2 * stop_pulse - min_pulse
+        kit.continuous_servo[slot].set_pulse_width_range(min_pulse, max_pulse)
+
+        kit.continuous_servo[slot].throttle = 0.3
+        time.sleep(1)
+
+        kit.continuous_servo[slot].throttle = 0
+        time.sleep(2)
+        ans = input(
+            f"Stop pulse: {stop_pulse} - Did the servo stop? Should I save the value? (y or n):"
+        )
+        if ans == "y":
+            data = {
+                f"{name}": {
+                    "min_pulse": min_pulse,
+                    "stop_pulse": stop_pulse,
+                    "max_pulse": max_pulse,
+                },
+            }
+
+            json.dump(data, open("ServoCalibration.json", "w"))
+        break
--- Deploy/ServoTest.py
+++ Deploy/ServoTest.py
@@ -1,52 +1,32 @@
+import json
 import time
 
 from adafruit_servokit import ServoKit
 
-calibrate = True
 continous_servo = True
-full_stop_pulse = 0
 
 
 def main():
-    kit = ServoKit(channels=16)
     print("This is a test. Will move servo on Channel 0")
     try:
-        if calibrate:
-            calibrateServo(kit)
-        elif continous_servo:
+        kit = ServoKit(channels=16)
+        if continous_servo:
             testContinousServo(kit)
         else:
             testServo(kit)
-        kit.continuous_servo[0].fraction = None
+        kit.continuous_servo[0].fraction = 0
 
     except KeyboardInterrupt:
         print("stopped")
-
-
-def calibrateServo(kit: ServoKit):
-    # This sweeps through 1480-1550µs in 10µ steps
-    for stop_pulse in range(1480, 1560, 10):
-        min_pulse = 600
-        max_pulse = 2 * stop_pulse - min_pulse
-        kit.continuous_servo[0].set_pulse_width_range(min_pulse, max_pulse)
-
-        kit.continuous_servo[0].throttle = 0.3
-        time.sleep(1)
-
-        kit.continuous_servo[0].throttle = 0
-        time.sleep(2)
-        print(f"Stop pulse: {stop_pulse}µs — did the servo stop?")
-        if kit.continuous_servo[0].fraction == 0:
-            ans = input(
-                "The servo seems to have stopped. Should I save the value? (y or n):"
-            )
-            if ans == "y":
-                full_stop_pulse = stop_pulse
-
     pass
 
 
 def testContinousServo(kit):
+    name = input("Enter Servo name: ")
+    min_pulse = json.load(open("ServoCalibration.json", "r"))[f"{name}"]["min_pulse"]
+    max_pulse = json.load(open("ServoCalibration.json", "r"))[f"{name}"]["max_pulse"]
+
+    kit.continous_servo[0].set_pulse_width_range(min_pulse, max_pulse)
     print("Setting power to -0.5")
     kit.continuous_servo[0].throttle = -0.5
     time.sleep(3)
