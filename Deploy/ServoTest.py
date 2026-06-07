ce4a5490b4ed42835fcabaac64a4c0b9f3de69d3
Pranav <pvanparty@gmail.com>
2026-06-06 20:57:08 -0400

    Added a way to change slots via input

--- Deploy/ServoTest.py
+++ Deploy/ServoTest.py
@@ -3,58 +3,64 @@ import time
 
 from adafruit_servokit import ServoKit
 
-continuous_servo = True
-
 
 def main():
-    print("This is a test. Will move servo on Channel 0")
+    print("This is a test.")
+
+    if (input("Is this a continuous servo? (t or f)")) == "t":
+        continuous_servo = True
+    else:
+        continuous_servo = False
+
+    slot = input("Enter slot number: ")
+
     try:
         kit = ServoKit(channels=16)
         if continuous_servo:
-            testContinuousServo(kit)
+            testContinuousServo(kit, slot)
         else:
-            testServo(kit)
+            testServo(kit, slot)
 
     except KeyboardInterrupt:
         print("stopped")
     pass
 
 
-def testContinuousServo(kit):
+def testContinuousServo(kit, slot):
     name = input("Enter Servo name: ")
     min_pulse = json.load(open("ServoCalibration.json", "r"))[f"{name}"]["min_pulse"]
     max_pulse = json.load(open("ServoCalibration.json", "r"))[f"{name}"]["max_pulse"]
 
-    kit.continuous_servo[0].set_pulse_width_range(min_pulse, max_pulse)
+    kit.continuous_servo[slot].set_pulse_width_range(min_pulse, max_pulse)
     print("Setting power to -0.5")
-    kit.continuous_servo[0].throttle = -0.5
+    kit.continuous_servo[slot].throttle = -0.5
     time.sleep(3)
 
     print("Stopping Servo")
-    kit.continuous_servo[0].throttle = 0
+    kit.continuous_servo[slot].throttle = 0
     time.sleep(3)
 
     print("Setting power to 0.5")
-    kit.continuous_servo[0].throttle = 0.5
+    kit.continuous_servo[slot].throttle = 0.5
     time.sleep(3)
 
     print("Stopping Servo")
-    kit.continuous_servo[0].throttle = 0
+    kit.continuous_servo[slot].throttle = 0
     time.sleep(3)
 
 
-def testServo(kit):
-    range = kit.servo[0].actuation_range
+def testServo(kit, slot):
+    range = kit.servo[slot].actuation_range
     print("Setting angle to 0º")
-    kit.servo[0].angle = 0
+    kit.servo[slot].angle = 0
     time.sleep(3)
 
     print("Setting angle to Midpoint")
-    kit.servo[0].angle = int(range / 2)
+    kit.servo[slot].angle = int(range / 2)
     time.sleep(3)
 
     print("Setting angle to Max")
-    kit.servo[0].angle = int(range)
+    kit.servo[slot].angle = int(range)
     time.sleep(3)
 
 
